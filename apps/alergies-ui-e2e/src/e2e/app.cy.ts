572a219e378e3abf34d343bd58ba92aeeed84623
echonabin <echonabin@gmail.com>
2022-12-19 14:57:07 +0545

    basic testing changed

--- apps/alergies-ui-e2e/src/e2e/app.cy.ts
+++ apps/alergies-ui-e2e/src/e2e/app.cy.ts
@@ -1,13 +1,13 @@
-import { getGreeting } from '../support/app.po';
+import { getLogin } from '../support/app.po';
 
 describe('alergies-ui', () => {
   beforeEach(() => cy.visit('/'));
 
-  it('should display welcome message', () => {
+  it('should redirect to /login and render login page', () => {
     // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
+    // cy.login('my-email@something.com', 'myPassword');
 
     // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome alergies-ui');
+    getLogin().contains('Login');
   });
 });
--- apps/alergies-ui-e2e/src/support/app.po.ts
+++ apps/alergies-ui-e2e/src/support/app.po.ts
@@ -1,1 +1,1 @@
-export const getGreeting = () => cy.get('h1');
+export const getLogin = () => cy.get('body');
