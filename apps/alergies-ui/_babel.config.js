303e9a33fb211f23d0cb47d45c6b901ea60a4be9
echonabin <echonabin@gmail.com>
2022-12-23 09:28:44 +0545

    coverage report generated with bug

--- apps/alergies-ui/_babel.config.js
+++ apps/alergies-ui/_babel.config.js
@@ -1,7 +1,7 @@
 module.exports = function (api, options) {
   api.assertVersion(7);
   return {
-    presets: [['next/babel', options]],
+    presets: [['@nrwl/next/babel', options]],
     plugins: [
       [require.resolve('@babel/plugin-proposal-decorators'), { legacy: true }],
       ['istanbul'],
