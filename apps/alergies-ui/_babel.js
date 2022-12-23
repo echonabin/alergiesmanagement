36dabd7f0ee0edc4542cdfb4c8f1637d3285100a
echonabin <echonabin@gmail.com>
2022-12-23 09:15:33 +0545

    babel config added

---
+++ apps/alergies-ui/_babel.js
@@ -0,0 +1,10 @@
+module.exports = function (api, options) {
+  api.assertVersion(7);
+  return {
+    presets: [['next/babel', options]],
+    plugins: [
+      [require.resolve('@babel/plugin-proposal-decorators'), { legacy: true }],
+      ['istanbul'],
+    ],
+  };
+};
